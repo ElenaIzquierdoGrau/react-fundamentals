78d7512bd954dba8ac654028c138ff49bc7617cc
Elena Izquierdo i Grau <elena.izquierdo@thoughtworks.com>
2022-11-30 12:16:43 +0100

    Solve exercise 5 with extra

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -13,19 +13,43 @@ import '../box-styles.css'
 // matches what the text says it should be
 // 🐨 also use the style prop to make the font italic
 // 💰 Here are available style attributes: backgroundColor, fontStyle
+function Box(props) {
+    const sizeClassName = props.size ? `box--${props.size}` : ''
+    return (
+        <div className={`box ${sizeClassName}`} style={props.style}>{props.message}</div>
+    )
 
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+}
+const smallBox = (
+    <Box
+         style={{fontStyle: 'italic', backgroundColor: 'lightblue'}}
+         message='small lightblue box'
+         size='small'
+    />
+)
+const mediumBox = (
+    <Box
+         style={{fontStyle: 'italic', backgroundColor: 'pink'}}
+         message='medium pink box'
+         size='medium'
+    />
+)
+const largeBox = (
+    <Box
+         style={{fontStyle: 'italic', backgroundColor: 'orange'}}
+         message='large orange box'
+         size='large'
+    />
+)
 
 function App() {
-  return (
-    <div>
-      {smallBox}
-      {mediumBox}
-      {largeBox}
-    </div>
-  )
+    return (
+        <div>
+            {smallBox}
+            {mediumBox}
+            {largeBox}
+        </div>
+    )
 }
 
 export default App
